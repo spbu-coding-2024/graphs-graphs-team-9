e2fced0fd119808157ef00055901dbc431b8ee7f
fUS1ONd <koskriv2006@gmail.com>
2025-05-01 10:22:30 +0300

    remove: removed test in main

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -9,7 +9,8 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.application
-import model.graph.Graph
+import model.graph.WeightedGraph
+import model.graph.Vertex
 
 @Composable
 @Preview
@@ -29,28 +30,4 @@ fun main() = application {
     Window(onCloseRequest = ::exitApplication) {
         App()
     }
-
-    var graph = Graph()
-    graph.addVertex(1, "A")
-    graph.addVertex(2, "B")
-    graph.addVertex(3, "C")
-    graph.addVertex(4, "D")
-    graph.addVertex(5, "E")
-    graph.addVertex(6, "F")
-    graph.addVertex(7, "G")
-
-    graph.addEdge(1, 2, 1.0)
-    graph.addEdge(1, 2, 1.0)
-    graph.addEdge(1, 2, 1.0)
-    graph.addEdge(1, 3, 2.0)
-    graph.addEdge(1, 4, 3.0)
-    graph.addEdge(1, 5, 4.0)
-    graph.addEdge(1, 6, 5.0)
-    graph.addEdge(1, 7, 6.0)
-
-//    graph.printG()
-
-    graph.deleteEdge(1, 2)
-
-    graph.printG()
 }
