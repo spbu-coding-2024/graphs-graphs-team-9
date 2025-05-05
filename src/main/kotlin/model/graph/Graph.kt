d6d36df71f97245461ebd4a851857c9043a74ca4
fUS1ONd <koskriv2006@gmail.com>
2025-05-06 01:53:33 +0300

    chore(Graph): get isDir.. and isWeight.. funcs

--- src/main/kotlin/model/graph/Graph.kt
+++ src/main/kotlin/model/graph/Graph.kt
@@ -1,4 +1,19 @@
 package model.graph
 
 interface Graph {
+    fun addVertex(vertex: Vertex)
+    fun removeVertex(vertex: Vertex)
+    fun addEdge(from: Vertex, to: Vertex, weight: Double? = null)
+    fun removeEdge(from: Vertex, to: Vertex)
+    fun getEdgeWeight(from: Vertex, to: Vertex): Double?
+    fun getNeighbors(vertex: Vertex): List<Vertex>
+    fun containsVertex(vertex: Vertex): Boolean
+    fun containsEdge(from: Vertex, to: Vertex): Boolean
+    fun getVertexCount(): Int
+    fun getEdgeCount(): Int
+    fun getVertices(): List<Vertex>
+    fun getEdges(): List<Edge>
+
+    fun isDirected(): Boolean
+    fun isWeighted(): Boolean
 }
