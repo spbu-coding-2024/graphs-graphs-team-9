c4f5b6c7ced9ab54cee1646e58779d7a74129d67
fUS1ONd <koskriv2006@gmail.com>
2025-05-09 13:15:41 +0300

    fix: changed jvm version to 18

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,7 +2,7 @@ import org.jetbrains.compose.desktop.application.dsl.TargetFormat
 
 plugins {
     kotlin("jvm") version "2.1.10"
-    id("org.jetbrains.compose")
+    id("org.jetbrains.compose") version "1.6.10"
     id("org.jetbrains.kotlin.plugin.compose")
     id("jacoco")
 }
@@ -17,16 +17,14 @@ repositories {
 }
 
 dependencies {
-    // Note, if you develop a library, you should use compose.desktop.common.
-    // compose.desktop.currentOs should be used in launcher-sourceSet
-    // (in a separate module for demo project and in testMain).
-    // With compose.desktop.common you will also lose @Preview functionality
     implementation(compose.desktop.currentOs)
-
     implementation("org.apache.commons:commons-lang3:3.12.0")
-
     testImplementation("org.junit.jupiter:junit-jupiter:5.9.3")
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
+}
+
+kotlin {
+    jvmToolchain(18)
 }
 
 tasks.test {
@@ -46,15 +44,10 @@ tasks.jacocoTestReport {
 compose.desktop {
     application {
         mainClass = "MainKt"
-
         nativeDistributions {
             targetFormats(TargetFormat.Dmg, TargetFormat.Msi, TargetFormat.Deb)
             packageName = "graphs-graphs-team-9"
             packageVersion = "1.0.0"
         }
     }
-}
-
-kotlin {
-    jvmToolchain(23)
 }
