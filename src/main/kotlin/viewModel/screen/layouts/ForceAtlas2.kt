59a1351b0f84d12954ec258b39f8ca0bf6ad4f4d
fUS1ONd <koskriv2006@gmail.com>
2025-05-29 16:09:21 +0300

    big change: merge 2 files into ForceAtlas2.kt

--- src/main/kotlin/viewModel/screen/layouts/ForceAtlas2.kt
+++ src/main/kotlin/viewModel/screen/layouts/ForceAtlas2.kt
@@ -1,48 +1,68 @@
 package viewModel.screen.layouts
 
-import model.graph.Graph
-import model.graph.Vertex
+import androidx.compose.ui.unit.dp
+import org.gephi.graph.api.GraphController
 import org.gephi.graph.api.Node
+import org.gephi.layout.plugin.forceAtlas2.ForceAtlas2
 import org.gephi.project.api.ProjectController
 import org.openide.util.Lookup
-import org.gephi.layout.plugin.forceAtlas2.ForceAtlas2 as GephiForceAtlas2
+import viewModel.graph.GraphViewModel
+import kotlin.math.abs
+import kotlin.random.Random
 
-class ForceAtlas2(private val graphModelForStrategy: Graph) : GephiAdapter(), RepresentationStrategy {
+class ForceAtlas2: RepresentationStrategy {
+    override fun layout(height: Double, width: Double, graphViewModel: GraphViewModel) {
 
-    override fun apply(graph: Graph): Map<Vertex, Pair<Float, Float>> {
         val pc: ProjectController = Lookup.getDefault().lookup(ProjectController::class.java)
         pc.newProject()
+        val workSpace = pc.currentWorkspace
+        val graphModel = Lookup.getDefault().lookup(GraphController::class.java).getGraphModel(workSpace)
+        val graph = graphModel.graph
 
-        // Поздняя инициализация graphModel и gephiGraph
-        this.graphModel = Lookup.getDefault().lookup(org.gephi.graph.api.GraphController::class.java).graphModel
-        this.gephiGraph = if (graph.isDirected()) this.graphModel.directedGraph else this.graphModel.undirectedGraph
+        val vertices = graphViewModel.vertices
+        val edges = graphViewModel.edges
 
-        val algorithm = GephiForceAtlas2(null)
-        val map: HashMap<Vertex, Node> = convertToGephi(graph)
+        val mapping = mutableMapOf<String, Node>()
 
-        algorithm.setGraphModel(this.graphModel)
+        for (vertex in vertices) {
+            val id = vertex.Id.toString()
+            val node: Node = graphModel.factory().newNode(id)
+            node.label = id
+            node.setX(abs(Random.nextFloat()*1000))
+            node.setY(abs(Random.nextFloat()*1000))
+            node.setSize(vertex.radius.value)
+            graph.addNode(node)
+            mapping[vertex.Id.toString()] = node
+        }
+
+        for (edge in edges) {
+            graph.addEdge(graphModel.factory().newEdge(mapping[edge.u.Id.toString()], mapping[edge.v.Id.toString()]))
+        }
+
+        var algorithm = ForceAtlas2(null)
+        algorithm.setGraphModel(graphModel)
+        algorithm.initAlgo()
         algorithm.resetPropertiesValues()
         algorithm.isBarnesHutOptimize = true
         algorithm.scalingRatio = 35.0
         algorithm.gravity = 1.5
         algorithm.isLinLogMode = true
 
-        algorithm.initAlgo()
-        repeat(100) {
+        for (i in 1..100) {
             if (algorithm.canAlgo()) {
                 algorithm.goAlgo()
             } else {
-                return@repeat
+                break
+            }
+
+            for (vertex in vertices) {
+                val m = mapping[vertex.Id.toString()]
+                val x = m?.x()?.dp ?: vertex.x
+                val y = m?.y()?.dp ?: vertex.y
+                vertex.x = x + (width / 2).dp
+                vertex.y = y + (height / 2).dp
             }
         }
         algorithm.endAlgo()
-
-        return convertBackWithXY(map)
-    }
-
-    override fun layout(
-            graph: Graph,
-    ): Map<Vertex, Pair<Float, Float>> {
-        return apply(graph)
     }
 }
