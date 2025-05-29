18a108ae54ce7c30ce396e963061d8815063bb65
fUS1ONd <koskriv2006@gmail.com>
2025-05-29 16:12:59 +0300

    chore(RepresStrategy.kt): new logic folayout
    Теперь функция layout интерфеса раскладки принимает graphViewModel
    В связи с этим последующие алгоритмы раскладки, должны обрабатывает динаты vertexViewModel

--- src/main/kotlin/viewModel/screen/layouts/RepresentationStrategy.kt
+++ src/main/kotlin/viewModel/screen/layouts/RepresentationStrategy.kt
@@ -1,15 +1,7 @@
 package viewModel.screen.layouts
 
-import model.graph.Graph
-import model.graph.Vertex
+import viewModel.graph.GraphViewModel
 
 interface RepresentationStrategy {
-    /**
-     * Рассчитывает "сырые" или относительные позиции для вершин графа.
-     * @param graph Граф для компоновки.
-     * @param canvasWidthHint Подсказка по ширине канвы (может использоваться алгоритмом).
-     * @param canvasHeightHint Подсказка по высоте канвы (может использоваться алгоритмом).
-     * @return Map, где ключ - это Vertex, а значение - Pair из X и Y Float координат.
-     */
-    fun layout(graph: Graph): Map<Vertex, Pair<Float, Float>>
+    fun layout(height: Double, width: Double, graphViewModel: GraphViewModel)
 }
